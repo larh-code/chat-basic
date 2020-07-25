5aa1ff6bc76ae1519cc90da848e1c16b103093c2
Luis Rivero <luis.arh.1109@gmail.com>
2020-07-25 16:40:05 -0400

    funcionalidad basica para comunicacion entre diferentes usuarios

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 const port = 3000;
-let users = [];
+let users = new Map();
 let rooms = [];
 let messages = [];
 
@@ -12,28 +12,36 @@ app.get('/', (req, res) => res.send('Hello World!'));
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  io.emit('new user connected', getUsers());
 
   // agregar usuario
   socket.on('new user', (data) => {
-    users.push(data.user);
-
-    socket.broadcast.emit('new user connected', users);
+    console.log('new user',data);
+    users.set(data.user.toLowerCase().trim(), data.user);
+    socket.broadcast.emit('new user connected', getUsers());
   });
 
   // crear chat entre usuario
   socket.on('new room', (data) => {
+    console.log('new room',data);
     rooms.push(data.room);
-
     socket.join(data.room);
   });
 
   // mensaje
   socket.on('message', data => {
+    console.log('message',data);
     messages.push(data);
-    io.in(data.room).emit('new message', {user: data.user, message: data.message});
+    io.in(data.room).emit('new message', data);
   })
 
   // salir del chat
 });
+
+function getUsers() {
+  const data = [];
+  users.forEach(user => data.push(user));
+  return data;
+}
 
 http.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
